0d5f3929a44046091c3c2d3b22583b83229ea2f2
Atogebania Alexander <aatogebania@gmail.com>
2024-10-16 13:30:03 +0000

    sdgg

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App(){
+    return (
+        <div>
+            <h1>First React App</h1>
+            <p>My first React app is working!</p>
+            <p>More text</p>
+        </div>
+    )
+}
+
+export default App;
